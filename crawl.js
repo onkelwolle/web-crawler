4ba60aa95c64b6e3d535db7eeec01858bb183d6c
Wolfgang Zimmermann <wolfgang.zimmermann@allianz.de>
2024-07-10 14:07:20 +0200

    feat: add function to normalize urls

---
+++ crawl.js
@@ -0,0 +1,11 @@
+export { normalizeURL };
+
+function normalizeURL(url) {
+        const urlObj = new URL(url)
+        let fullPath = `${urlObj.host}${urlObj.pathname}`
+
+        if (fullPath.slice(-1) === '/') {
+                fullPath = fullPath.slice(0, -1)
+        }
+        return fullPath
+}
