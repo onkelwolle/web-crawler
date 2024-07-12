9af4895d5779d9c5b1e009a1578ef49f479b580f
Wolfgang Zimmermann <wolfgang@zimmermann.red>
2024-07-12 11:45:16 +0200

    feat: add ability to read cli arguments and use it as base url

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+import { argv } from 'node:process';
+
+function main() {
+
+        if (argv.length !== 3) {
+                console.log('Not enough or too many arguments, accepting min 1 and max 1 argument');
+                return;
+        }
+
+        console.log(`Base URL is: ${argv[2]}`);
+}
+
+main()
+
